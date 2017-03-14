e50ef3b5bf243f9c42f93541e335d94d7367d656
reo <devloprk555@gmail.com>
2017-03-14 22:06:58 +0900

    selenium 対応

--- src/test/java/jp/co/test/devwork/selenium_webdriver_sample/AppTest.java
+++ src/test/java/jp/co/test/devwork/selenium_webdriver_sample/AppTest.java
@@ -1,38 +1,33 @@
 package jp.co.test.devwork.selenium_webdriver_sample;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.net.URL;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class AppTest {
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities chrome = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chrome);
+        driver.get("https://www.google.co.jp/");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testCase() throws Exception {
+        Thread.sleep(10000);
+
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
     }
 }
